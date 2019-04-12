ff1f732694df7dec8ff09e6035fc4eac94c352c0
Antoine Cully <a.cully@imperial.ac.uk>
2019-04-12 13:50:33 +0100

    fix the multi-user scenario

--- start_container.sh
+++ start_container.sh
@@ -1,6 +1,6 @@
 LOCAL_EXP_PATH=~/'git'
 IMAGENAME='aneoshun/airl_env:latest'
-CONTAINERNAME='airl_env'
+CONTAINERNAME="airl_env_$USER"
 NOVNC_IP=6081
 
 
@@ -22,7 +22,7 @@ done
 echo "Visualisation available after activating the visu_server.sh script at the http://localhost:$NOVNC_IP/"
 if [ ! "$(docker ps -q -f name=^/$CONTAINERNAME)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=^/$CONTAINERNAME)" ]; then
-        docker start -ai airl_env
+        docker start -ai $CONTAINERNAME
     else
 	docker run --privileged --name=$CONTAINERNAME -p $NOVNC_IP:6080 -m 8GB -it  -v $LOCAL_EXP_PATH:/git/sferes2/exp  $IMAGENAME
     fi
