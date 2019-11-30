f430e543253d99e2a2f202fb720a0c775fdbd79c
Antoine Cully <a.cully@imperial.ac.uk>
2019-11-30 23:01:16 +0000

    changes in script

--- .gitlabci/setup.sh
+++ .gitlabci/setup.sh
@@ -1,33 +1,8 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y wget git \
-                                          build-essential \
-                                          squashfs-tools \
-                                          libtool \
-                                          autotools-dev \
-                                          libarchive-dev \
-                                          automake \
-                                          autoconf \
-                                          uuid-dev \
-                                          libssl-dev
-
-
-sed -i -e 's/^Defaults\tsecure_path.*$//' /etc/sudoers
-
 # Check Python
 
 echo "Python Version:"
 python --version
-pip install sregistry[all]
-sregistry version
+pip install hpccm
 
-echo "sregistry Version:"
-
-# Install Singularity
-
-cd /tmp && \
-    git clone -b release-3.4 https://www.github.com/sylabs/singularity.git
-    cd singularity && \
-    ./autogen.sh && \
-    ./configure --prefix=/usr/local && \
-    make && make install
