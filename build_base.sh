f21c80d40f80c094540006e9880d777b2e0137a6
Antoine Cully <a.cully@imperial.ac.uk>
2019-12-01 15:46:00 +0000

    add option to build without fakeroot

--- build_base.sh
+++ build_base.sh
@@ -1,6 +1,35 @@
 #!/usr/bin/env bash
 
-python3 airl_env_base.py > hppcm_airl_env_base.def
-cat hppcm_airl_env_base.def
-singularity build --notest --fakeroot hppcm_airl_env_base.sif hppcm_airl_env_base.def
+# idiomatic parameter and option handling in sh
+while test $# -gt 0
+do
+    case "$1" in
+        --nofakeroot) nofakeroot=true
+            ;;
+        --*) echo "bad option $1"
+            ;;
+        *) echo "argument $1"
+            ;;
+    esac
+    shift
+done
 
+build_option=""
+if [ "$nofakeroot" != true ]; then
+    echo "using FAKEROOT"
+    build_option="--fakeroot"
+fi
+
+
+python3 airl_env_base.py > hppcm_airl_env_base.def
+ret=$?
+if [ $ret -ne 0 ]; then
+    echo "Generation of the definition file failed."
+    exit $ret
+fi
+
+cat hppcm_airl_env_base.def
+
+singularity build --notest $build_option hppcm_airl_env_base.sif hppcm_airl_env_base.def
+
+exit 0
