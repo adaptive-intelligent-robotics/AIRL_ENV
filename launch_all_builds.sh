3f159a65fa8e6014a476634a05322f6d24bec940
Luca Grillotti <luca.grillotti16@imperial.ac.uk>
2021-03-03 08:07:39 +0000

    Adding script for building all images at once

---
+++ launch_all_builds.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+./build_image_without_ci.sh --basename airl_env_base
+./build_image_without_ci.sh --basename airl_env_pytorch
+./build_image_without_ci.sh --basename airl_env_bare
+
