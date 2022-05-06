5a70fbe5adc033809cb82b9f7dd6ed0c4d5b7856
Manon Flageat <manon.flageat18@imperial.ac.uk>
2022-05-06 16:01:26 +0000

    Rename brax to qdax + add to gitlab-ci

--- launch_all_builds.sh
+++ launch_all_builds.sh
@@ -3,4 +3,5 @@
 ./build_image_without_ci.sh --basename airl_env_base
 ./build_image_without_ci.sh --basename airl_env_pytorch
 ./build_image_without_ci.sh --basename airl_env_bare
+./build_image_without_ci.sh --basename airl_env_qdax
 
