8bc00bf6ca01fc9b683bca278c214cc3765425bb
Antoine Cully <a.cully@imperial.ac.uk>
2023-05-08 14:27:37 +0000

    transition to github

--- .gitlabci/launch_exp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "CI_COMMIT_REF_PROTECTED: $CI_COMMIT_REF_PROTECTED"
-curl  --request POST --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" $CI_API_V4_URL/projects/$CI_PROJECT_ID/repository/commits/$CI_COMMIT_SHORT_SHA/discussions --data-urlencod "body=This message has been automatically generation by CI"
