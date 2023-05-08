94ea099e3f4d85bf8dc410a3a186764c7f32be54
Antoine Cully <a.cully@imperial.ac.uk>
2023-05-08 15:53:49 +0100

    Update ci_script.sh

--- .ci/ci_script.sh
+++ .ci/ci_script.sh
@@ -35,6 +35,6 @@ echo $basename
 echo "tag:"
 echo $tag
 
-/bin/bash ./.gitlabci/build_image.sh --basename $basename --nofakeroot 
-/bin/bash ./.gitlabci/test_image.sh --imagefile $basename.sif --nofakeroot 
-if [ $CI_COMMIT_REF_NAME = "master" ]; then /bin/bash ./.gitlabci/push_image.sh --uri library://airl_lab/default/airl_env --tag $tag --cli registry --imagefile $basename.sif  ; else echo "NOT on master branch, not pushing"; fi; 
+/bin/bash ./.ci/build_image.sh --basename $basename --nofakeroot 
+/bin/bash ./.ci/test_image.sh --imagefile $basename.sif --nofakeroot 
+if [ $CI_COMMIT_REF_NAME = "master" ]; then /bin/bash ./.ci/push_image.sh --uri library://adaptive-intelligent-robotics/AIRL_ENV --tag $tag --cli ghcr --imagefile $basename.sif  ; else echo "NOT on master branch, not pushing"; fi; 
