59363e4422630c0b95ffca4d3add7b1583397426
Antoine Cully <a.cully@imperial.ac.uk>
2023-05-08 18:06:17 +0100

    Update ci_script.sh

--- .ci/ci_script.sh
+++ .ci/ci_script.sh
@@ -15,6 +15,11 @@ while true; do
             basename="${1:-}"
             shift
         ;;
+        -k|--token)
+            shift
+            token="${1:-}"
+            shift
+        ;;
         \?) printf "illegal option: -%s\n" "${1:-}" >&2
             usage
             exit 1
@@ -38,4 +43,4 @@ echo $tag
 /bin/bash ./.ci/build_image.sh --basename $basename --nofakeroot 
 # /bin/bash ./.ci/test_image.sh --imagefile $basename.sif --nofakeroot 
 #if [ $CI_COMMIT_REF_NAME = "master" ]; then 
-/bin/bash ./.ci/push_image.sh --uri library://adaptive-intelligent-robotics/airl_env/$basename --tag $tag --cli ghcr --imagefile $basename.sif  #; else echo "NOT on master branch, not pushing"; fi; 
+/bin/bash ./.ci/push_image.sh --uri library://adaptive-intelligent-robotics/airl_env/$basename --tag $tag --cli ghcr --imagefile $basename.sif --token $token #; else echo "NOT on master branch, not pushing"; fi; 
